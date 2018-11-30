5e34ba72ef309c10bfcbff2fe8df7d0b31b61025
Logan <logan.dewilfond@gmail.com>
2018-11-30 13:20:13 -0600

    update

---
+++ while do or do while/while do or do while/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace while_do_or_do_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            while(i <= 9)
+            {
+                WriteLine(i);
+                i++;
+            }
+            while (i >= 0)
+            {
+                WriteLine(i);
+                i--;
+            }
+            do
+            {
+                WriteLine(i);
+                i++;
+            } while (i <= 5);
+
+            for (int j = 0; j <= 5; ++j)
+            {
+                WriteLine(j);
+            }
+        }
+    }
+}
