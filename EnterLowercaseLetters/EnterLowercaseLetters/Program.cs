91325920c76c682c662d0bc48c6226ce7ad452d2
Logan <logan.dewilfond@gmail.com>
2018-12-03 13:09:57 -0600

    update.

---
+++ EnterLowercaseLetters/EnterLowercaseLetters/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace EnterLowercaseLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter;
+            int exit;
+            do
+            {
+                WriteLine("Enter a lowercase letter. ");
+                letter = Convert.ToChar(ReadLine());
+
+                if (letter >= 'a' && letter <= 's')
+                {
+                    WriteLine("Character is lowercase");
+                }
+                else
+                    WriteLine("ERROR");
+
+            } while (letter == '!');
+        }
+    }
+}
---
+++ SumInts/SumInts/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace SumInts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int sum = 0;
+            string inputString;
+
+            do
+            {
+                WriteLine("Enter a number or to exit 999");
+                inputString = ReadLine();
+                num = Convert.ToInt32(inputString);
+                sum = sum + num;
+                WriteLine("The sum is " + sum);
+            } while (num != 999);
+        }
+    }
+}
