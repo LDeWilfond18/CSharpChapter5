dba8680b75482fb6e9a31c368c6bbdfd47354f27
Logan <logan.dewilfond@gmail.com>
2018-12-06 12:51:48 -0600

    h

---
+++ GuessingGame3/GuessingGame3/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace GuessingGame3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = 1;
+            int max = 11;
+            int againL = 1;
+            int againH = 11;
+            int count = 0;
+            string guessString;
+            int guess = 9000;
+            string result = null;
+
+            Random ranNumberGenerator = new Random();
+            int randomNumber;
+            randomNumber = ranNumberGenerator.Next(min, max);
+            do
+            {
+                WriteLine("Choose a number between 1-10 >>> ");
+                guessString = ReadLine();
+                guess = Convert.ToInt32(guessString);
+
+                if (guess <= 10 && guess >= 1)
+                {
+                    if (randomNumber == guess)
+                    {
+                        result = "Congratulations on the guess of " + randomNumber;
+                    }
+                    else if (guess < randomNumber)
+                    {
+                        result = "You guessed " + guess + " to low";
+                        if (guess <= againL)
+                        {
+                            result = "What?! " + guess + " Why? You stoopid";
+                        }
+                        againL = guess;
+                    }
+                }
+                else if (guess > randomNumber)
+                {
+                    result = "You guessed " + guess + " to high";
+                    if (guess >= againH)
+                    {
+                        result = "What?! " + guess + " Why? You stoopid";
+                    }
+                    againH = guess;
+                }
+                else if (guess < min || guess >= max)
+                {
+                    result = "wth are you doing buddy that wasn't in the guidelines";
+                }
+                DisplayString(result);
+                count++;
+
+            } while (guess != randomNumber);
+            result = "You guessed " + guess + " in " + count + " tries.";
+            DisplayString(result);
+        }
+        static void DisplayString(string result)
+        {
+            WriteLine(result);
+        }
+    }
+}
---
+++ OddNums/OddNums/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace OddNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            do
+            {
+                num = num + 2;
+                WriteLine(num);
+            } while (num != 99);
+        }
+    }
+}
